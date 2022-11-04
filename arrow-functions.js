b49b772bd58237ae6525d13d2be55e97b503d2e8
PeterHolmqvist <peterholmqvist@hotmail.com>
2022-11-04 12:40:40 +0000

    first commit

--- arrow-functions.js
+++ arrow-functions.js
@@ -3,12 +3,38 @@
  * command node arrow-functions.js in the terminal
  */
 
-// Vanilla JavaScript Function
+/* 
+ Vanilla JavaScript Function
+function addTwoNumbers(a, b) {
+    return a + b;
+}
+let sum = addTwoNumbers(3, 5)
+console.log(sum)
+*/
 
 // Arrow Function With Parameters
+const addTwoNumbers = (a, b) => {
+    return a + b;
+}
+let sum = addTwoNumbers(5, 4)
+console.log(sum)
+
 
 // Single Line Arrow Function With Parameters
+/*const addTwoNumbers2 = (a, b)   => a + b; !also valid */
+const addTwoNumbers2 = (a, b)   => a + b;
+let sum2 = addTwoNumbers2(6, 8)
+console.log(sum2)
 
 // Implicit Returns
+saySomething = message => console.log(message);
+saySomething('hello world')
+
+const sayHello = () => console.log('hello!');
+sayHello()
 
 // Returning Multiple Lines
+const returnMultipleLines = () => console.log(`<p>
+    say hello to my little friend...
+</p>`);
+returnMultipleLines()
